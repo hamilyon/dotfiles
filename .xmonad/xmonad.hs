749069c6a806e8123aa7511d22d95552b8bb73cb
A.Shaposhnikov <a.shaposhnikov@hh.ru>
2013-08-20 12:01:08 +0400

    Xmonad chromium fullscreen

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -16,10 +16,12 @@ module Main (main) where
     import XMonad.Layout.Magnifier
     import XMonad.Layout.HintedTile
     import XMonad.Layout.NoBorders
+    import XMonad.Hooks.EwmhDesktops
 
     main :: IO ()
-    main = xmonad $ gnomeConfig { keys = newKeys 
+    main = xmonad $ gnomeConfig { keys = newKeys
      , manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig
+     , handleEventHook    = fullscreenEventHook
      }
 
     myManageHook = composeAll
@@ -30,5 +32,5 @@ module Main (main) where
 
     myKeys conf@(XConfig {XMonad.modMask = modm}) =
              [ ((modm, xK_F12), xmonadPrompt defaultXPConfig)
-             , ((modm, xK_F2 ), shellPrompt  defaultXPConfig), ((modm, xK_F3 ), spawn "exe=`dmenu_path | dmenu -fn '-*-terminus-*-*-*-*-12-*-*-*-*-*-*-u' -nb '#1c1c1c' -nf '#C4C4C4' -sf '#D7AF87'` && eval \"exec $exe\"")
-	     ]
+              , ((modm, xK_F2 ), shellPrompt defaultXPConfig), ((modm, xK_F3 ), spawn "exe=`dmenu_path | dmenu -fn '-*-terminus-*-*-*-*-12-*-*-*-*-*-*-u' -nb '#1c1c1c' -nf '#C4C4C4' -sf '#D7AF87'` && eval \"exec $exe\"")
+             ]
